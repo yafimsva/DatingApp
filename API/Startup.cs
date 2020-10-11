96ee1e3c2cd9e59ef0b8ddb0f5f98a9c9eeadd5d
Yafim Vainilovich <yafimsva@gmail.com>
2020-10-11 16:05:42 -0700

    Added jwt security and password hashing, and login and register restful methods

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,18 +1,10 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using API.Data;
+using API.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
 
 namespace API
 {
@@ -27,16 +19,10 @@ namespace API
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
-      services.AddDbContext<DataContext>(options =>
-      {
-        options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
-      });
+      services.AddApplicationServices(_config);
       services.AddControllers();
       services.AddCors();
-      services.AddSwaggerGen(c =>
-      {
-        c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-      });
+      services.AddIdentityServices(_config);
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,8 +31,6 @@ namespace API
       if (env.IsDevelopment())
       {
         app.UseDeveloperExceptionPage();
-        app.UseSwagger();
-        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
       }
 
       app.UseHttpsRedirection();
@@ -54,6 +38,8 @@ namespace API
       app.UseRouting();
 
       app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+
+      app.UseAuthentication();
 
       app.UseAuthorization();
 
